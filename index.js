ba80bf0217420678e36a45a4b8a961bba5cd11c5
Lam0509 <112493001+Lam0509@users.noreply.github.com>
2022-09-07 13:29:30 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Bài 1
+
+// let a = parseInt(prompt("Nhập giá trị a"));
+// let b = parseInt(prompt("Nhập giá trị b"));
+//
+// let c = ((a + b) < 4) ? "Below" : "Over";
+//
+// document.write(c);
+
+// Bài 2
+
+// let mess = prompt("Nhập tin nhắn");
+// (mess == "Employee") ?
+//     alert("Hello") :
+//     (mess == "Director") ?
+//         alert("Greetings") :
+//         (mess == "") ?
+//             alert("No login") :
+//             alert("Đây là chuỗi rỗng")
+
